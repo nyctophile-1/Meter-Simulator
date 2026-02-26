5eb233ba2bdc74cf067da9372d2819b286e88037
Akshita Gupta <akshita.gupta@kimbal.io>
2026-02-26 12:42:05 +0530

    Adding Setup for 100 meters

---
+++ Meter-Simulator/Config/DeleteNameplate.sql
@@ -0,0 +1,22 @@
+﻿DECLARE @MeterCount INT = 100; 
+DECLARE @StartIndex INT = 1;
+
+DECLARE @i INT = @StartIndex;
+
+WHILE @i < @StartIndex + @MeterCount
+BEGIN
+    DECLARE @MeterNo VARCHAR(20) = 'MTR' + RIGHT('00000' + CAST(@i AS VARCHAR), 5);
+
+    DECLARE @NodeId VARCHAR(20) = CAST(10000000 + @i AS VARCHAR); 
+
+    DELETE FROM [LatestRouting]
+    WHERE NodeId = @NodeId;
+
+    DELETE FROM [MeterSecurity]
+    WHERE MeterNo = @MeterNo;
+
+    DELETE FROM [NamePlate]
+    WHERE MeterNo = @MeterNo;
+
+    SET @i = @i + 1;
+END
